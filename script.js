d46f30cd29b09ec0ac4f9a9386186085d3068eb3
ANTONY VALENTIM GARCIA DOS SANTOS <antony_v_santos@estudante.sesisenai.org.br>
2025-03-05 10:15:23 -0300

    [ADDING] Flying cube

--- script.js
+++ script.js
@@ -1,13 +1,36 @@
 const canvas = document.getElementById("jogo2D");
 const ctx = canvas.getContext('2d'); // Inicializando canvas no contexto 2D (largura e altura)
+document.addEventListener('keypress', (e) => {
+    if(e.code == 'Space'){
+        personagem.velocidadeY = -15;
+        personagem.pulando = true;
+    }
+})
+const personagem = {
+    x: 100,
+    y: canvas.height - 50,
+    altura: 50,
+    largura: 50,
+    velocidadeY: 0,
+    pulando: false
+}
 
 function deseharPersonagem(){
     ctx.fillStyle = 'blue'
-    ctx.fillRect(150, 100, 50, 50);
+    ctx.fillRect(personagem.x, personagem.y, personagem.altura, personagem.largura);
+};
+
+function atualizaPersonagem(){
+    if(personagem.pulando == true){
+        personagem.y += personagem.velocidadeY
+    }
 };
 
 function loop() {
-    deseharPersonagem()
+    ctx.clearRect(0, 0, canvas.width, canvas.height); // Limpa a tela
+    deseharPersonagem(); // Desenha de novo
+    atualizaPersonagem();
+    requestAnimationFrame(loop); // Atualiza posições
 };
 
 loop();
