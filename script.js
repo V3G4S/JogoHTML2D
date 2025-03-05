b0063ab99422cec53f7908a35502f622c0c028f0
ANTONY VALENTIM GARCIA DOS SANTOS <antony_v_santos@estudante.sesisenai.org.br>
2025-03-05 11:04:39 -0300

    [FUNCTION] Jumping cube

--- script.js
+++ script.js
@@ -1,36 +1,61 @@
-const canvas = document.getElementById("jogo2D");
-const ctx = canvas.getContext('2d'); // Inicializando canvas no contexto 2D (largura e altura)
-document.addEventListener('keypress', (e) => {
-    if(e.code == 'Space'){
-        personagem.velocidadeY = -15;
-        personagem.pulando = true;
+const canvas = document.getElementById('jogo2D');
+const ctx = canvas.getContext('2d');
+const gravidade = 1
+
+document.addEventListener('keydown', (e) => {
+    if(e.code == 'Space' && personagem.pulo == false) {
+        console.log("Press the Space Bar")
+        personagem.velocidadeY = 15
+        personagem.pulo = true
     }
 })
+
 const personagem = {
     x: 100,
-    y: canvas.height - 50,
-    altura: 50,
-    largura: 50,
-    velocidadeY: 0,
-    pulando: false
+    y:canvas.height - 50,
+    height: 50,
+    width: 50,
+    pulo: false,
+    velocidadeY: 0
 }
 
-function deseharPersonagem(){
+function desenharPersonagem(){
     ctx.fillStyle = 'blue'
-    ctx.fillRect(personagem.x, personagem.y, personagem.altura, personagem.largura);
-};
+    ctx.fillRect(personagem.x,personagem.y,personagem.height,personagem.width);
+}
 
-function atualizaPersonagem(){
-    if(personagem.pulando == true){
-        personagem.y += personagem.velocidadeY
+function pularpersonagem(){
+    if(!personagem.pulo) return
+        
+    personagem.y -= personagem.velocidadeY
+    personagem.velocidadeY -= gravidade
+    if(personagem.y < canvas.height - 50) return;
+    
+    personagem.velocidadeY = 0
+    personagem.pulo = false
+    personagem.y = canvas.height-50
+}
+
+function atualizarPersonagem(){
+    if(personagem.pulo == true){
+        personagem.y -= personagem.velocidadeY;
+        personagem.velocidadeY -= gravidade
+        if(personagem.y >= canvas.height-50){
+            personagem.velocidadeY = 0
+            personagem.pulo = false
+            personagem.y = canvas.height-50
+        }
     }
-};
+}
 
-function loop() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height); // Limpa a tela
-    deseharPersonagem(); // Desenha de novo
-    atualizaPersonagem();
-    requestAnimationFrame(loop); // Atualiza posições
-};
+function loop(){
+    ctx.clearRect(0,0,canvas.width,canvas.height);{
 
-loop();
+    }
+
+    desenharPersonagem();
+    atualizarPersonagem()
+    requestAnimationFrame(loop)
+}
+
+loop()
