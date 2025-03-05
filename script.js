825c5962faec6a38542251158f22fc72ce938f62
ANTONY VALENTIM GARCIA DOS SANTOS <antony_v_santos@estudante.sesisenai.org.br>
2025-03-05 09:40:04 -0300

    [UPDATE] New colors

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 const canvas = document.getElementById("jogo2D");
 const ctx = canvas.getContext('2d'); // Inicializando canvas no contexto 2D (largura e altura)
 
-function loop() {
+function deseharPersonagem(){
+    ctx.fillStyle = 'blue'
+    ctx.fillRect(150, 100, 50, 50);
+};
 
+function loop() {
+    deseharPersonagem()
 };
 
 loop();
